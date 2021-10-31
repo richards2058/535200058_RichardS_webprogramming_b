ee463f0dd08a5aa44610bafc1bbe840778c60cbd
richards2058 <richard.535200058@stu.untar.ac.id>
2021-10-31 11:10:26 +0700

    tugas 10

--- TUGAS9_535200058_Richard_Stephen/index.js
+++ TUGAS9_535200058_Richard_Stephen/index.js
@@ -3,19 +3,19 @@ const express = require("express");
 var app = express();
 
 app.get("/", function (req, res) {
-  res.send("Hello World!");
+    res.send("Hello World!");
 });
 
 app.post("/", function (req, res) {
-  res.send("Ini POST Request!");
+    res.send("Ini POST Request!");
 });
 
 app.put("/user", function (req, res) {
-  res.send("PUT Request dijalankan!");
+    res.send("PUT Request dijalankan!");
 });
 
 app.delete("/user", function (req, res) {
-  res.send("DELETE Request pada suatu user!");
+    res.send("DELETE Request pada suatu user!");
 });
 
 app.use(express.static("public"));
